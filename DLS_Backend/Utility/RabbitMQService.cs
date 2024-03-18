cd840b4137379ee1350c9da6ac821f998af55859
Zack Ottesen <zo@pyra.dk>
2024-03-18 11:20:43 +0100

    setup connection to rabbitMQ

---
+++ DLS_Backend/Utility/RabbitMQService.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+namespace DLS_Backend.utility;
+using System.Text;
+
+public class RabbitMQService
+{
+    private readonly ConnectionFactory _factory;
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+
+    public RabbitMQService()
+    {
+        _factory = new ConnectionFactory()
+        {
+            HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST"),
+            UserName = Environment.GetEnvironmentVariable("RABBITMQ_USERNAME"),
+            Password = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD")
+        };
+        _connection = _factory.CreateConnection();
+        _channel = _connection.CreateModel();
+
+        _channel.QueueDeclare(queue: "testQueue",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+    }
+
+    public void SendMessage(string message)
+    {
+        var body = Encoding.UTF8.GetBytes(message);
+
+        _channel.BasicPublish(exchange: "",
+            routingKey: "testQueue",
+            basicProperties: null,
+            body: body);
+    }
+
+    public void Close()
+    {
+        _connection.Close();
+    }
+}
