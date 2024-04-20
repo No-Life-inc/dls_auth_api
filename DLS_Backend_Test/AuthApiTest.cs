c8ec47da9fa3eb74a983695c006eca769bb84e4f
ZackO <zo@pyra.dk>
2024-04-20 11:33:46 +0200

    debug

--- DLS_Backend_Test/AuthApiTest.cs
+++ DLS_Backend_Test/AuthApiTest.cs
@@ -137,7 +137,7 @@ public class ApiTests : IClassFixture<WebApplicationFactory<Program>>
     
     private void InitializeTestDatabase(string server, string database, string user, string password)
     {
-        var connectionString = $"Server=mssql,1433;Database=master;User=SA;Password=YourNewPassword123;TrustServerCertificate=True;";
+        var connectionString = $"Server=mssql;Database=master;User=SA;Password=YourNewPassword123;TrustServerCertificate=True;";
         Console.WriteLine(password);
         Console.WriteLine(connectionString);
         using var connection = new SqlConnection(connectionString);
